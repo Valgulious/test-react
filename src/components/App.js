a23e85c3773dde188dd67702f891f8771de389b3
Valgulious <dmitryfedorenkof@gmail.com>
2021-04-14 17:28:48 +1000

    add Aside

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,13 @@
 import '../css/App.css';
 import Header from "./header/Header.jsx";
+import Aside from "./aside/Aside.jsx";
 
 function App() {
   return (
-    <Header/>
+    <>
+      <Header/>
+      <Aside/>
+    </>
   );
 }
 
