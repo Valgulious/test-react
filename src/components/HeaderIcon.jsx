8326052611ee07bc1fef2344cf9ce7314ac08326
Valgulious <dmitryfedorenkof@gmail.com>
2021-04-15 11:41:13 +1000

    add HeaderIcon component

---
+++ src/components/HeaderIcon.jsx
@@ -0,0 +1,14 @@
+import styled from "@emotion/styled";
+
+const HeaderIcon = styled.img`
+        width: 20px;
+        height: 20px;
+        padding: 5px;
+        border-radius: 2px;
+        cursor: pointer;
+        &:hover {
+            background-color: #3c4a97
+        }
+    `
+
+export default HeaderIcon
